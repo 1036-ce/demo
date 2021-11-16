dbdfb947712eeb83b7d048c1a5a0cf130c29d318
1036-ce <2376030467@qq.com>
2021-11-16 19:05:40 +0800

    demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int n,m;
+int ans; 
+void dfs(int h)
+{
+	if(h==m)
+		return;
+}
+int main(void)
+{
+	scanf("%d%d",&n,&m);
+	dfs(0);
+	printf("%d",ans);
+	return 0;
+}
