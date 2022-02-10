2350b07aa7c7b8184ac9d838fdde7881a9776f5a
1036-ce <2376030467@qq.com>
2022-02-11 02:00:11 +0800

    ad

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,21 @@
+#include <string>
+#include <iostream>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
 
 using namespace std;
 
-int main()
+void swap(int &a, int &b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+int main() 
 {
+	int a = 12, b = 23;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	swap(a,b);
+	cout<<a<<endl;
+	cout<<b<<endl;
 	return 0;
 }
-
